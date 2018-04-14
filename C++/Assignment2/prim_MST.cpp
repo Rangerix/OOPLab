8438d11d03f6e7f42f253aebf44a65bc97f13de9
rangerix <kushalkanti1999@gmail.com>
2018-04-14 11:47:44 +0530

    Prim's algorithm to print Minimum Spanning Tree using STL priority queue

--- C++/Assignment2/prim_MST.cpp
+++ C++/Assignment2/prim_MST.cpp
@@ -1,63 +1,66 @@
 #include <iostream>
 #include <algorithm>
 #include <queue>
+#include <stack>
 #include <vector>
 using namespace std;
 
 
-typedef long int tt;
-typedef pair<tt,tt> ii;
+typedef pair<int,int> ii;
+int store[10000][3]={0};
+int global_index=0;
 
-vector<tt> taken;
-priority_queue<ii> pq;          //max heap by default; we have to use - sign for our work
+vector<int> taken;
+int index[10000]={0};
+priority_queue<pair<int,ii> > pq;          //max heap by default; we have to use - sign for our work
 vector<vector<ii> > AdjList;
 void process(int vrtx)
 {
     taken[vrtx]=1;
-    for(tt i=0;i<AdjList[vrtx].size();i++){
-        ii v = AdjList[vrtx][i];
-        if(!taken[v.first])
-            pq.push(ii(-v.second,-v.first));
+    for(int i=0;i<AdjList[vrtx].size();i++){
+        ii p = AdjList[vrtx][i];
+        int w=-p.second;
+        int v=p.first;
+        ii p1=make_pair(vrtx,v);
+        if(!taken[v]){
+            pq.push(make_pair(w,p1));
+        }
     }
 }
 
 int main()
 {
-    tt vrtx,edge,i,j,a,b,w,s,u,v;
+    int vrtx,edge,i,j,a,b,w,s,u,v;
     cout<<"Enter how many vertex : ";
     cin>>vrtx;
     cout<<"Enter how many edges : ";
     cin>>edge;
     AdjList.assign(vrtx,vector<ii> () );
-    cout<<"Enter start point , end point , and weight";
+    cout<<"Enter start point , end point , and weight : "<<endl;
     for(i=0;i<edge;i++)
     {
         cin>>a>>b>>w;
-        AdjList[a-1].push_back(ii(b-1,w));
-        AdjList[b-1].push_back(ii(a-1,w));
+        AdjList[a].push_back(ii(b,w));
+        AdjList[b].push_back(ii(a,w));
     }
-    vector<int> result;
+    int cost=0;
     taken.assign(vrtx,0);
-    cin>>s;                                         //Though no need of source; As we can strart with any vertex
-    result.push_back(s-1);
-    process(s-1);
-    tt cost=0;
-    
-    while(!pq.empty())
-    {
-        ii tmp=pq.top();
+    process(0);
+    cout<<endl;
+    while(!pq.empty()){
+        pair<int,ii> temp=pq.top();
         pq.pop();
-        u = -tmp.second;
-        w = -tmp.first;
-        if(!taken[u]){
-            result.push_back(u);
-            cost += w;
-            process(u);
+        int w=-temp.first;
+        ii p=temp.second;
+        int u=p.first;
+        int v=p.second;
+        if(!taken[v]){
+            cost+=w;
+            cout<<u<<" - "<<v<<" : "<<w<<endl;
+            process(v);
         }
     }
-    for(int result_count=0;result_count<result.size();result_count++)
-        cout<<result[result_count]<<" ";
-    cout<<endl;
-    cout<<cost<<endl;
+    cout<<"\nTotal cost : "<<cost<<endl;
+
 return 0;
 }
